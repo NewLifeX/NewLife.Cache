609022e46457297c197eef95d7973547306b76ee
智能大石头 <nnhy@vip.qq.com>
2022-07-21 07:52:23 +0800

    所有接口以Packet作为出入参，避免Json序列化，最大程度提升性能

--- NewLife.CacheServer/Program.cs
+++ NewLife.CacheServer/Program.cs
@@ -1,73 +1,72 @@
-﻿using System;
-using NewLife.Agent;
+﻿using NewLife.Agent;
 using NewLife.Caching;
 using NewLife.Log;
 using NewLife.Remoting;
 
-namespace NewLife.CacheServer
+namespace NewLife.CacheServer;
+
+internal class Program
 {
-    class Program
+    private static void Main(String[] args) => new MyService().Main(args);
+
+    private class MyService : ServiceBase
     {
-        static void Main(String[] args) => new MyService().Main(args);
-
-        class MyService : ServiceBase
+        public MyService()
         {
-            public MyService()
+            ServiceName = "CacheServer";
+            DisplayName = "缓存服务器";
+        }
+
+        private ApiServer _Server;
+        protected override void StartWork(String reason)
+        {
+            // 配置
+            var set = Setting.Current;
+
+            // 服务器
+            var svr = new ApiServer(set.Port)
             {
-                ServiceName = "CacheServer";
-                DisplayName = "缓存服务器";
-            }
+                //Encoder = new BinaryEncoder(),
+                Log = XTrace.Log,
+                ShowError = true,
+            };
 
-            private ApiServer _Server;
-            protected override void StartWork(String reason)
-            {
-                // 配置
-                var set = Setting.Current;
+            if (set.Debug) svr.EncoderLog = XTrace.Log;
 
-                // 服务器
-                var svr = new ApiServer(set.Port)
-                {
-                    //Encoder = new BinaryEncoder(),
-                    Log = XTrace.Log,
-                    ShowError = true,
-                };
-
-                if (set.Debug) svr.EncoderLog = XTrace.Log;
-
-                // 网络层日志
-                var ns = svr.EnsureCreate() as Net.NetServer;
-                ns.SessionLog = XTrace.Log;
-                ns.SocketLog = XTrace.Log;
+            // 网络层日志
+            var ns = svr.EnsureCreate() as Net.NetServer;
+            ns.SessionLog = XTrace.Log;
+            ns.SocketLog = XTrace.Log;
 #if DEBUG
-                ns.LogSend = true;
-                ns.LogReceive = true;
+            //ns.LogSend = true;
+            //ns.LogReceive = true;
+            svr.EncoderLog = XTrace.Log;
 #endif
 
-                // 统计日志
-                svr.StatPeriod = 10;
+            // 统计日志
+            svr.StatPeriod = 10;
 
-                // 缓存提供者
-                var mc = new MemoryCache();
-                if (set.Expire > 0) mc.Expire = set.Expire;
+            // 缓存提供者
+            var mc = new MemoryCache();
+            if (set.Expire > 0) mc.Expire = set.Expire;
 
-                // 注册RPC服务
-                var svc = new CacheService { Cache = mc };
-                svr.Register(svc, null);
+            // 注册RPC服务
+            var svc = new CacheService { Cache = mc };
+            svr.Register(svc, null);
 
-                svr.Start();
+            svr.Start();
 
-                _Server = svr;
+            _Server = svr;
 
-                base.StartWork(reason);
-            }
+            base.StartWork(reason);
+        }
 
-            protected override void StopWork(String reason)
-            {
-                _Server.TryDispose();
-                _Server = null;
+        protected override void StopWork(String reason)
+        {
+            _Server.TryDispose();
+            _Server = null;
 
-                base.StopWork(reason);
-            }
+            base.StopWork(reason);
         }
     }
 }
--- Test/Program.cs
+++ Test/Program.cs
@@ -4,74 +4,78 @@ using System.Linq;
 using System.Threading;
 using NewLife;
 using NewLife.Caching;
+using NewLife.Data;
 using NewLife.Log;
 using NewLife.Reflection;
 using NewLife.Serialization;
 
-namespace Test
+namespace Test;
+
+internal class Program
 {
-    class Program
+    private static void Main(String[] args)
     {
-        static void Main(String[] args)
+        XTrace.UseConsole();
+
+        try
         {
-            XTrace.UseConsole();
-
-            try
-            {
-                Test1();
-            }
-            catch (Exception ex)
-            {
-                XTrace.WriteException(ex);
-            }
-
-            Console.WriteLine("OK");
-            Console.Read();
+            Test1();
+        }
+        catch (Exception ex)
+        {
+            XTrace.WriteException(ex);
         }
 
-        static void Test1()
-        {
-            var client = new CacheClient();
-            client.SetServer("tcp://127.0.0.1:1234");
+        Console.WriteLine("OK");
+        Console.Read();
+    }
+
+    private static void Test1()
+    {
+        var client = new CacheClient();
+        client.SetServer("tcp://127.0.0.1:1234");
 
 #if !DEBUG
-            client.Bench();
+        client.Bench();
 #else
-            Debug.Assert(client.Set("aa", 1234));
-            Debug.Assert(client.ContainsKey("aa"));
-            Debug.Assert(client.Get<Int32>("aa") == 1234);
+        client.Client.EncoderLog = XTrace.Log;
 
-            client.Set("bb", false);
-            client.Set("cc", 3.14);
-            client.Set("dd", "NewLife", 5);
-            client.Set("ee", new { Name = "新生命", Year = 2002 });
+        Console.WriteLine("缓存个数：{0}", client.Count);
 
-            Console.WriteLine(client.Get<Int32>("aa"));
-            Console.WriteLine(client.Get<Boolean>("bb"));
-            Console.WriteLine(client.Get<Double>("cc"));
-            Console.WriteLine(client.Get<String>("dd"));
-            Console.WriteLine(client.Get<Object>("ee").ToJson());
+        Debug.Assert(client.Set("aa", 1234));
+        Debug.Assert(client.ContainsKey("aa"));
+        Debug.Assert(client.Get<Int32>("aa") == 1234);
 
-            Console.WriteLine();
-            Console.WriteLine("Count={0}", client.Count);
-            Console.WriteLine("Keys={0}", client.Keys.Join(","));
-            Thread.Sleep(2000);
-            Console.WriteLine("Expire={0}", client.GetExpire("dd"));
+        client.Set("bb", false);
+        client.Set("cc", 3.14);
+        client.Set("dd", "NewLife", 5);
+        client.Set("ee", new { Name = "新生命", Year = 2002 });
 
-            Console.WriteLine();
-            client.Decrement("aa", 30);
-            client.Increment("cc", 0.3);
+        Console.WriteLine(client.Get<Int32>("aa"));
+        Console.WriteLine(client.Get<Boolean>("bb"));
+        Console.WriteLine(client.Get<Double>("cc"));
+        Console.WriteLine(client.Get<String>("dd"));
+        Console.WriteLine(client.Get<Packet>("ee").ToStr());
 
-            Console.WriteLine();
-            var dic = client.GetAll<Object>(new[] { "aa", "cc", "ee" });
-            foreach (var item in dic)
-            {
-                var val = item.Value;
-                if (val != null && item.Value.GetType().GetTypeCode() == TypeCode.Object) val = val.ToJson();
+        Console.WriteLine();
+        Console.WriteLine("Count={0}", client.Count);
+        Console.WriteLine("Keys={0}", client.Keys.Join(","));
+        Thread.Sleep(2000);
+        Console.WriteLine("Expire={0}", client.GetExpire("dd"));
 
-                Console.WriteLine("{0}={1}", item.Key, val);
-            }
-#endif
+        Console.WriteLine();
+        client.Decrement("aa", 30);
+        client.Increment("cc", 0.3);
+
+        Console.WriteLine();
+        var dic = client.GetAll<Object>(new[] { "aa", "cc", "ee" });
+        foreach (var item in dic)
+        {
+            var val = item.Value;
+            if (val != null && item.Value.GetType().GetTypeCode() == TypeCode.Object) val = val.ToJson();
+
+            Console.WriteLine("{0}={1}", item.Key, val);
         }
+#endif
     }
 }
